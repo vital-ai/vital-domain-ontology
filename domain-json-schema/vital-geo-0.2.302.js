36700f6f6fd989ea21b43544910ad39d1cb9d7ef
Derek Kobylarz <darek.kobylarz@gmail.com>
2016-02-03 01:39:48 +0100

    vital domains bumped to 0.2.302

---
+++ domain-json-schema/vital-geo-0.2.302.js
@@ -0,0 +1,215 @@
+if(typeof(VITAL_JSON_SCHEMAS) == 'undefined') {
+ throw ("No VITAL_JSON_SCHEMAS list defined - vital-core domain unavailable");
+}
+
+var vital_geo_0_2_302_schema = {
+  "domainURI" : "http://vital.ai/ontology/vital-geo",
+  "name" : "vital-geo-0.2.302",
+  "vitalsignsVersion" : "0.2.302",
+  "parents" : [ "http://vital.ai/ontology/vital" ],
+  "schemas" : [ {
+    "id" : "http://vital.ai/ontology/vital-geo#Country",
+    "parent" : "http://vital.ai/ontology/vital-core#VITAL_Node",
+    "$schema" : "http://json-schema.org/draft-04/schema#",
+    "title" : "http://vital.ai/ontology/vital-geo#Country",
+    "description" : "Auto-generated schema for owl class: http://vital.ai/ontology/vital-geo#Country",
+    "type" : "object",
+    "properties" : {
+      "URI" : {
+        "type" : "string"
+      },
+      "type" : {
+        "enum" : [ "http://vital.ai/ontology/vital-geo#Country" ]
+      },
+      "types" : {
+        "type" : "array",
+        "minItems" : 1,
+        "items" : {
+          "type" : "string"
+        },
+        "uniqueItems" : true
+      },
+      "http://vital.ai/ontology/vital#hasCertainty" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasDegree" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasPageRank" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasProvenance" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasName" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasOntologyIRI" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasTimestamp" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital-core#hasVersionIRI" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#isActive" : {
+        "type" : "boolean"
+      },
+      "http://vital.ai/ontology/vital-geo#hasCountryCode2" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-geo#hasCountryCode3" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-geo#hasCountryCodeNumeric" : {
+        "type" : "string"
+      }
+    },
+    "required" : [ "URI", "type" ],
+    "additionalProperties" : false
+  }, {
+    "id" : "http://vital.ai/ontology/vital-geo#USState",
+    "parent" : "http://vital.ai/ontology/vital-core#VITAL_Node",
+    "$schema" : "http://json-schema.org/draft-04/schema#",
+    "title" : "http://vital.ai/ontology/vital-geo#USState",
+    "description" : "Auto-generated schema for owl class: http://vital.ai/ontology/vital-geo#USState",
+    "type" : "object",
+    "properties" : {
+      "URI" : {
+        "type" : "string"
+      },
+      "type" : {
+        "enum" : [ "http://vital.ai/ontology/vital-geo#USState" ]
+      },
+      "types" : {
+        "type" : "array",
+        "minItems" : 1,
+        "items" : {
+          "type" : "string"
+        },
+        "uniqueItems" : true
+      },
+      "http://vital.ai/ontology/vital#hasCertainty" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasDegree" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasPageRank" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasProvenance" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasName" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasOntologyIRI" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasTimestamp" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital-core#hasVersionIRI" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#isActive" : {
+        "type" : "boolean"
+      },
+      "http://vital.ai/ontology/vital-geo#hasUSStateCode" : {
+        "type" : "string"
+      }
+    },
+    "required" : [ "URI", "type" ],
+    "additionalProperties" : false
+  }, {
+    "id" : "http://vital.ai/ontology/vital-geo#ZipCode",
+    "parent" : "http://vital.ai/ontology/vital-core#VITAL_Node",
+    "$schema" : "http://json-schema.org/draft-04/schema#",
+    "title" : "http://vital.ai/ontology/vital-geo#ZipCode",
+    "description" : "Auto-generated schema for owl class: http://vital.ai/ontology/vital-geo#ZipCode",
+    "type" : "object",
+    "properties" : {
+      "URI" : {
+        "type" : "string"
+      },
+      "type" : {
+        "enum" : [ "http://vital.ai/ontology/vital-geo#ZipCode" ]
+      },
+      "types" : {
+        "type" : "array",
+        "minItems" : 1,
+        "items" : {
+          "type" : "string"
+        },
+        "uniqueItems" : true
+      },
+      "http://vital.ai/ontology/vital#hasCertainty" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasDegree" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasPageRank" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital#hasProvenance" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasName" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasOntologyIRI" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#hasTimestamp" : {
+        "type" : "number"
+      },
+      "http://vital.ai/ontology/vital-core#hasVersionIRI" : {
+        "type" : "string"
+      },
+      "http://vital.ai/ontology/vital-core#isActive" : {
+        "type" : "boolean"
+      },
+      "http://vital.ai/ontology/vital-geo#hasZipCode" : {
+        "type" : "string"
+      }
+    },
+    "required" : [ "URI", "type" ],
+    "additionalProperties" : false
+  } ],
+  "properties" : [ {
+    "URI" : "http://vital.ai/ontology/vital-geo#hasCountryCode2",
+    "domainClassesURIs" : [ "http://vital.ai/ontology/vital-geo#Country" ],
+    "shortName" : "countryCode2",
+    "multipleValues" : false,
+    "type" : "StringProperty"
+  }, {
+    "URI" : "http://vital.ai/ontology/vital-geo#hasCountryCode3",
+    "domainClassesURIs" : [ "http://vital.ai/ontology/vital-geo#Country" ],
+    "shortName" : "countryCode3",
+    "multipleValues" : false,
+    "type" : "StringProperty"
+  }, {
+    "URI" : "http://vital.ai/ontology/vital-geo#hasCountryCodeNumeric",
+    "domainClassesURIs" : [ "http://vital.ai/ontology/vital-geo#Country" ],
+    "shortName" : "countryCodeNumeric",
+    "multipleValues" : false,
+    "type" : "StringProperty"
+  }, {
+    "URI" : "http://vital.ai/ontology/vital-geo#hasUSStateCode",
+    "domainClassesURIs" : [ "http://vital.ai/ontology/vital-geo#USState" ],
+    "shortName" : "uSStateCode",
+    "multipleValues" : false,
+    "type" : "StringProperty"
+  }, {
+    "URI" : "http://vital.ai/ontology/vital-geo#hasZipCode",
+    "domainClassesURIs" : [ "http://vital.ai/ontology/vital-geo#ZipCode" ],
+    "shortName" : "zipCode",
+    "multipleValues" : false,
+    "type" : "StringProperty"
+  } ]
+};
+
+ VITAL_JSON_SCHEMAS.push(vital_geo_0_2_302_schema);
